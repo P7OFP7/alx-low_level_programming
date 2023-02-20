319ca5598f4ee607d78051c8ea80a828c5088505
P7OFP7 <boahengyanprosper@gmail.com>
2023-02-20 06:57:37 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
- * Return: 0
+ * main - Determines if a number is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,10 +13,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
-	else if (n < 0)
+	}
+	else if (n == 0)
+	{
 		printf("%i is negative\n", n);
+	}
 	else
+	{
 		printf("%i is zero\n", n);
+	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,8 @@
+project done during **Full stack software Engineering studie
+s** at **ALX School**. It aims to learn about to about arithmetic operators, relational operators, boolean operators, comments, declaring variables and loops in **C language**
+
+## technologies
+* Scripts written in Bash 4.3.11(1)
+* C files are compiled using gcc 4.8.4
+* C files are written according to the C90 standard
+* Tested on Ubuntu 14.4 LTS
